73ad5ab3238c7112dcc0d6fb1df023cb92f79f3c
eeqi98 <lim.ee@northeastern.edu>
2024-03-20 19:14:56 -0700

    a5 Kanbas

---
+++ Kanbas/Database/assignments.js
@@ -0,0 +1,137 @@
+export default [
+    {
+        "_id": "A101",
+        "title": "A1 - ENV + HTML",
+        "course": "RS101",
+        "due": "2024-09-18T23:59",
+        "point": "100"
+    },
+    {
+        "_id": "A102",
+        "title": "A2 - CSS + BOOTSTRAP",
+        "course": "RS101",
+        "due": "2024-10-02T23:59",
+        "point": "100"
+    },
+    {
+        "_id": "A103",
+        "title": "A3 - JS + REACT",
+        "course": "RS101",
+        "due": "2024-10-16T23:59",
+        "point": "100"
+    },
+    {
+        "_id": "A104",
+        "title": "A4 - STATE + REDUX",
+        "course": "RS101",
+        "due": "2024-10-30T23:59",
+        "availableFrom": "2024-10-15T00:00",
+        "point": "100"
+    },
+    {
+        "_id": "A105",
+        "title": "A5 - NODE + SESSION",
+        "course": "RS101",
+        "due": "2024-11-13T23:59",
+        "availableFrom": "2024-10-09T00:00",
+        "point": "100"
+    },
+    {
+        "_id": "A106",
+        "title": "A6 - MONGO",
+        "course": "RS101",
+        "due": "2024-11-27T23:59",
+        "availableFrom": "2024-11-26T00:00",
+        "point": "100"
+    },
+    {
+        "_id": "A201",
+        "title": "A1 - ENV + HTML",
+        "course": "RS102",
+        "due": "2024-09-18T23:59",
+        "point": "100"
+    },
+    {
+        "_id": "A202",
+        "title": "A2 - CSS + BOOTSTRAP",
+        "course": "RS102",
+        "due": "2024-10-02T23:59",
+        "point": "100"
+    },
+    {
+        "_id": "A203",
+        "title": "A3 - JS + REACT",
+        "course": "RS102",
+        "due": "2024-10-16T23:59",
+        "point": "100"
+    },
+    {
+        "_id": "A204",
+        "title": "A4 - STATE + REDUX",
+        "course": "RS102",
+        "due": "2024-10-30T23:59",
+        "availableFrom": "2024-10-15T00:00",
+        "point": "100"
+    },
+    {
+        "_id": "A205",
+        "title": "A5 - NODE + SESSION",
+        "course": "RS102",
+        "due": "2024-11-13T23:59",
+        "availableFrom": "2024-10-09T00:00",
+        "point": "100"
+    },
+    {
+        "_id": "A206",
+        "title": "A6 - MONGO",
+        "course": "RS102",
+        "due": "2024-11-27T23:59",
+        "availableFrom": "2024-11-26T00:00",
+        "point": "100"
+    },
+    {
+        "_id": "A301",
+        "title": "A1 - ENV + HTML",
+        "course": "RS103",
+        "due": "2024-09-18T23:59",
+        "point": "100"
+    },
+    {
+        "_id": "A302",
+        "title": "A2 - CSS + BOOTSTRAP",
+        "course": "RS103",
+        "due": "2024-10-02T23:59",
+        "point": "100"
+    },
+    {
+        "_id": "A303",
+        "title": "A3 - JS + REACT",
+        "course": "RS103",
+        "due": "2024-10-16T23:59",
+        "point": "100"
+    },
+    {
+        "_id": "A304",
+        "title": "A4 - STATE + REDUX",
+        "course": "RS103",
+        "due": "2024-10-30T23:59",
+        "availableFrom": "2024-10-15T00:00",
+        "point": "100"
+    },
+    {
+        "_id": "A305",
+        "title": "A5 - NODE + SESSION",
+        "course": "RS103",
+        "due": "2024-11-13T23:59",
+        "availableFrom": "2024-10-09T00:00",
+        "point": "100"
+    },
+    {
+        "_id": "A306",
+        "title": "A6 - MONGO",
+        "course": "RS103",
+        "due": "2024-11-27T23:59",
+        "availableFrom": "2024-11-26T00:00",
+        "point": "100"
+    }
+]
