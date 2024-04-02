0d547615753201f0807b40094c72759e6c8c1a45
eeqi98 <lim.ee@northeastern.edu>
2024-04-02 13:05:57 -0700

    a6 Mongo

--- App.js
+++ App.js
@@ -1,17 +1,44 @@
 import express from 'express'
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import cors from "cors";
+import session from "express-session";
+import "dotenv/config";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 app.use(express.json());
 Lab5(app);
 Hello(app)
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
